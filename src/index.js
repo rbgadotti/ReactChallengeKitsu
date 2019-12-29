6614892f31f7c8ef235246138060a3c1a67a743d
Rafael Gadotti <rbgadotti@gmail.com>
2019-12-29 13:15:54 -0200

    bug fix - remove spinner

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
